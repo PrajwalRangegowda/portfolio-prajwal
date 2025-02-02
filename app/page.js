111ef875f1f34fb41818ca3a8c8c3cbb892a48cb
Prajwal Rangegowda <147972135+PrajwalRangegowda@users.noreply.github.com>
2025-02-02 12:21:05 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <IntroSection/>
+        {/* this is comment */}
       </main>
    
     </div>
